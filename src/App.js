5d73b78a4eb18f880bb5564ebdb282740f440cbb
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-07 20:23:32 +0900

    install prop-types, Add Food.propTypes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 const foodILike = [
     {
         id: 1,
         name: "Kimchi",
-        image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+        rating : 5
     }, {
         id: 2,
         name: "Samgyeopsal",
         image: "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSf" +
-                "HS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "HS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+        rating : 4.9
     }, {
         id: 3,
         name: "Bibimbap",
         image: "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_120" +
-                "0x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "0x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+        rating : 4.8
     }, {
         id: 4,
         name: "Doncasu",
-        image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+        rating : 5.5
     }, {
         id: 5,
         name: "Kimbap",
         image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512" +
-                "295.jpg"
+      "295.jpg",
+        rating : 4.7
     }
 ];
 
-function Food({name, picture}) {
+function Food({name, picture, rating}) {
     return <div>
-        <h1>I like {name}</h1>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.00</h4>
         <img alt={name} src={picture}/>
     </div>
 
 }
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+};
 
 function App() {
     return (
@@ -42,7 +54,7 @@ function App() {
             <h1>Hello</h1>
             {
                 foodILike.map(
-                    item => <Food key={item.id} name={item.name} picture={item.image}/>
+                  item => <Food key={item.id} name={item.name} picture={item.image} rating={item.rating}/>
                 )
             }
         </div>
