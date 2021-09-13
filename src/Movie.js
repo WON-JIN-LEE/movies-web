cec1209a48b5ece325b30d75bfc60cf87cbe207d
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-13 19:27:25 +0900

    Create App.css Add Styles Timelapse

--- src/Movie.js
+++ src/Movie.js
@@ -3,18 +3,20 @@ import PropTypes from "prop-types";
 import "./Movie.css"
 
 function Movie({ year, title, summary, poster, genres}) {
-    return <div className="movie">
+    return (
+        <div className="movie">
         <img src={poster} alt={title} title={title}/>
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres">{genres.map((genres, index) => (
-                <li key="index" className="genres_genre">{genres}</li>
+            <ul className="movie__genres">{genres.map((genres, index) => (
+                <li key={index} className="genres_genre">{genres}</li>
             ))}</ul>
             <p className="movie__summary">{summary}</p>
         </div>
 
-    </div>
+        </div>
+    )
 
 }
 
