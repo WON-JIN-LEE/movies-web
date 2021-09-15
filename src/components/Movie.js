2501f1e3e87168df09810cd440ea4e9cd4381646
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-15 23:43:19 +0900

    Update

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,6 @@ import "./Movie.css"
 
 function Movie({year, title, summary, poster, genres}) {
     return (<div className="movie">
-      
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
